ad83299e24cc8e0a739fc135451d9d41dd4a4de0
Jet-Green <grishadzyin@gmail.com>
2021-12-25 21:23:13 +0400

    resize and scaling quality, but with bugs

--- src/components/AddBook.vue
+++ src/components/AddBook.vue
@@ -15,13 +15,23 @@
             counter
             maxlength="300"
             full-width
-          ></v-textarea>
-
-          <v-text-field
-            v-model="form.CoverPath"
-            label="Фотография обложки"
             required
-          ></v-text-field>
+          ></v-textarea>
+          <v-form>
+            <v-file-input
+              label="Фотография обложки"
+              v-model="form.CoverPath"
+              required
+              accept="image/*"
+            ></v-file-input>
+            <v-btn @click="uploadPhoto">загрузить</v-btn>
+            <canvas id="preview-on-canvas"></canvas>
+            <v-divider></v-divider>
+            <div id="img-pre" />
+          </v-form>
+
+          <!-- <canvas id="preview-image"></canvas>
+          <div id="testing"></div> -->
 
           <v-text-field
             v-model="form.Authors"
@@ -120,6 +130,8 @@ export default {
         PageCount: null,
         Series: null,
       },
+      CoverPathImage: null,
+      editedImage: null,
     };
   },
   methods: {
@@ -147,7 +159,7 @@ export default {
       };
       axios
         .post("http://localhost:8080/api/books/create", this.form, {
-          headers: headers
+          headers: headers,
         })
         .then((response) => {
           console.log(response);
@@ -156,6 +168,102 @@ export default {
         .catch((error) => {
           console.log(error);
         });
+    },
+    uploadPhoto: function () {
+      let vm = this;
+      if (vm.form.CoverPath) {
+        console.log(vm.form.CoverPath);
+        const reader = new FileReader();
+        reader.readAsDataURL(vm.form.CoverPath);
+
+        // let preview = document.getElementById("img-pre");
+
+        reader.onloadend = function () {
+          vm.CoverPathImage = reader.result;
+          vm.resizeImage(vm);
+        };
+      } else {
+        console.log("No provided images");
+      }
+    },
+    resizeImage: async function (vm) {
+      const width = 400;
+      const height = 500;
+      const canvas = document.getElementById("preview-on-canvas");
+      canvas.width = width;
+      canvas.height = height;
+
+      const ctx = canvas.getContext("2d");
+
+      let img = new Image(width, height);
+      img.src = vm.CoverPathImage;
+      if (img.complete) {
+        ctx.drawImage(img, 0, 0);
+      } else {
+        img.onload = function () {
+          ctx.drawImage(img, 0, 0);
+        };
+      }
+      const data = await canvas.toDataURL("image/jpeg", 0.1);
+      const editedImg = new Image();
+      // if (data.complete) {
+      //   editedImg.src = data;
+      // } else {
+      //   data.onload = function () {
+
+      //   };
+      // }
+      editedImg.src = data;
+      const editedImagePreview = document.getElementById("img-pre");
+      if (editedImg.complete) {
+        // console.log(editedImg);
+        editedImagePreview.appendChild(editedImg);
+      } else {
+        editedImg.onload = function () {
+          // console.log(editedImg);
+          editedImagePreview.appendChild(editedImg);
+        };
+      }
+
+      return;
+      if (this.form.CoverPath) {
+        const reader = new FileReader();
+        reader.readAsDataURL(this.form.CoverPath);
+        // const fileName = this.form.CoverPath.name.replace(/\.[^/.]+$/, "");
+
+        reader.onload = (event) => {
+          const width = 200;
+          const height = 1000;
+          const img = new Image(width, height);
+          img.src = event.target.result;
+          let test = document.getElementById("testing");
+
+          let canvas = document.getElementById("preview-image");
+
+          // canvas.width = width;
+          // canvas.height = height;
+          const ctx = canvas.getContext("2d");
+          ctx.drawImage(img, 0, 0);
+          const data = canvas.toDataURL("image/png", 0.7);
+          this.previewImage = data;
+          // console.log(data);
+          // ctx.canvas.toBlob(
+          //   (blob) => {
+          //     // console.log(blob); //output image as a blob
+          //     const file = new File([blob], fileName + ".jpeg", {
+          //       type: "image/jpeg",
+          //       lastModified: Date.now(),
+          //     }); //output image as a file
+          //     /**Файл есть, осталось его сохранить */
+
+          //   },
+          //   "image/jpeg",
+          //   0.7
+          // );
+        };
+      } else {
+        console.log("No CoverPath images provided");
+      }
     },
   },
 };
