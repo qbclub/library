63531fd34fba8357c864bb5ec5e8b9c52727ffa2
Jet-Green <grishadzyin@gmail.cm>
2021-12-27 18:03:52 +0400

    fix routing from mini book

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="book">
     <v-container>
-      <span class="text-h6">{{ books[bookId].authors }}:{{ books[bookId].name }} </span>
+      <span class="text-h6"
+        >{{ currentBook.authors }}: {{ currentBook.name }}
+      </span>
       <v-row class="mt-2">
         <v-col>
           <v-container>
@@ -11,20 +13,23 @@
                   max-width="250"
                   :aspect-ratio="9 / 16"
                   contain
-                  :src="books[bookId].image"
+                  :src="currentBook.image"
                 ></v-img> </v-col
               ><v-col
-                ><div class="text-caption">Автор: {{ books[bookId].authors }}</div>
-                <div class="text-caption">
-                  Издательство: {{ books[bookId].publisher }}, {{ books[bookId].date }} г.
+                ><div class="text-caption">
+                  Автор: {{ currentBook.authors }}
                 </div>
                 <div class="text-caption">
-                  Тематика: {{ books[bookId].direction }}
+                  Издательство: {{ currentBook.publisher }},
+                  {{ currentBook.date }} г.
+                </div>
+                <div class="text-caption">
+                  Тематика: {{ currentBook.direction }}
                 </div>
 
-                <div class="text-caption">ISBN: {{ books[bookId].ISBN }}</div>
+                <div class="text-caption">ISBN: {{ currentBook.ISBN }}</div>
                 <div class="text-caption">
-                  Страниц: {{ books[bookId].pagesCount }}
+                  Страниц: {{ currentBook.pagesCount }}
                 </div>
                 <v-btn
                   depressed
@@ -33,7 +38,7 @@
                   v-on:click="takeBook"
                   >Взять книгу</v-btn
                 >
-                <div class="">Состояние: {{ books[bookId].state }}</div>
+                <div class="">Состояние: {{ currentBook.state }}</div>
               </v-col>
             </v-row>
           </v-container>
@@ -41,9 +46,9 @@
 
         <v-col>
           <div class="text-caption">
-            <b>Аннотация к книге "{{ books[bookId].name }}"</b>
+            <b>Аннотация к книге "{{ currentBook.name }}"</b>
           </div>
-          <span class="text-caption">{{ books[bookId].annotation }}</span>
+          <span class="text-caption">{{ currentBook.annotation }}</span>
         </v-col>
       </v-row>
     </v-container>
@@ -55,6 +60,8 @@ import books from "../db/books";
 export default {
   data: () => ({
     books,
+    currentBook: {},
+    bookId: 1,
   }),
   methods: {
     takeBook: function () {
@@ -63,7 +70,9 @@ export default {
   },
   mounted() {
     this.bookId = this.$route.query.book_id;
-  }
+    this.currentBook = this.books.find((x) => x.id == this.bookId);
+    console.log(this.currentBook);
+  },
 };
 </script>
 
