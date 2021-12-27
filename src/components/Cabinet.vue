c052727dd5ecff2d0a6116ca4532c6401bbada26
tooortttt <savelijsutov@gmail.com>
2021-12-27 19:19:35 +0400

    update cabinet

--- src/components/Cabinet.vue
+++ src/components/Cabinet.vue
@@ -15,7 +15,14 @@
       ></v-row
     >
     <v-row class="align-center justify-center">
-      <v-col cols="10"
+      <v-col
+        cols="6"
+        class="ma-0"
+        v-for="(bookid, i) in TakenBooks"
+        :key="i"
+        sm="4"
+        md="3"
+        lg="3"
         ><div
           class="
             text-lg-body-1
@@ -25,14 +32,30 @@
             text-center
           "
         >
-          Список книг
+          <v-img
+            max-width="250"
+            :aspect-ratio="9 / 16"
+            contain
+            :src="books[i].image"
+          ></v-img>
         </div>
       </v-col>
     </v-row>
   </v-container>
 </template>
 <script>
-export default {};
+import books from "../db/books";
+import users from "../db/users";
+export default {
+  data: () => ({
+    books,
+    users,
+    TakenBooks: [],
+  }),
+  mounted() {
+    this.TakenBooks = this.users[0].CurrentTakenBooks;
+  },
+};
 </script>
 <style scoped>
 </style>
