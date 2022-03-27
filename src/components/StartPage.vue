94302b90c9da92a23c6f253f1a8ee74e4d65bd95
qbclub <grachevrv@ya.ru>
2022-03-27 10:34:15 +0400

    start page img

--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
-     <p> Это библиотека клуба программирования Кубит</p>
-      <v-btn to="/books">Библиотека</v-btn>
+    <v-containner class="startPage d-flex align-center">
+      <v-row>
+        <v-col class="col-12 col-sm-6 d-flex justify-center flex-column align-center">
+      
+            <p>Это библиотека клуба программирования Кубит</p>
+            <v-btn  text color="accent"  to="/books">Вход</v-btn>
+         
+        </v-col>
+        <v-col class="col-12 col-sm-6 d-flex justify-center align-center">
+          <div class="d-flex justify-center">
+            <v-img
+              loading="lazy"
+             contain
+              
+              src="../assets/image/test.png"
+            ></v-img>
+          </div>
+        </v-col>
+      </v-row>
+    </v-containner>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.startPage {
+  height: calc(100vh - 200px);
+}
 </style>
