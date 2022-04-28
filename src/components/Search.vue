88ef3571d6963efe3c9708a3054435525fe09469
qbclub <grachevrv@ya.ru>
2022-04-28 19:24:49 +0400

    add filter

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,20 +1,36 @@
 <template>
   <div>
-    <v-text-field
-   
-      append-icon="mdi-search"
-      v-model="searchRequest"
-      label="Поиск"
-      color="accent"
-    ></v-text-field>
+    <v-row>
+      <v-col class="col-6 col-sm-8">
+        <v-text-field
+          append-icon="mdi-search"
+          v-model="searchRequest"
+          label="Поиск"
+          color="accent"
+        ></v-text-field>
+      </v-col>
+
+      <v-col class="col-6 col-sm-4">
+        <v-select
+          v-model="bookStatus"
+          :items="status"
+          label="Фильтр"
+          color="accent"
+        
+          prepend-icon="mdi-filter"
+        ></v-select>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
+    bookStatus: "Все",
     searchRequest: "",
     booksToShow: [],
+    status: ["Все", "На месте", "Зарезервирована", "Выдана"],
   }),
   props: {
     books: {
@@ -26,9 +42,11 @@ export default {
   },
   methods: {
     findAllBooks: function () {
-      this.searchRequest == ""
-        ? (this.booksToShow = this.books)
-        : (this.booksToShow = this.books.filter(
+      if (this.searchRequest == "" && this.bookStatus == "Все") {
+        this.booksToShow = this.books;
+      } else {
+        this.booksToShow = this.books
+          .filter(
             (book) =>
               book.Name.toLowerCase().includes(
                 this.searchRequest.toLowerCase()
@@ -42,7 +60,12 @@ export default {
               book.Sections.toLowerCase().includes(
                 this.searchRequest.toLowerCase()
               )
-          ));
+          )
+          .filter((book) =>
+            this.bookStatus == "Все" ? true : book.Status == this.bookStatus
+          );
+      }
+
       this.$emit("findBooks", this.booksToShow);
     },
   },
@@ -50,9 +73,15 @@ export default {
     searchRequest: function () {
       this.findAllBooks();
     },
+    bookStatus: function () {
+      this.findAllBooks();
+    },
   },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.v-list-item__content {
+  color: black !important;
+}
 </style>
